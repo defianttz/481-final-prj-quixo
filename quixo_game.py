5afc843cc77196db37904846534bf4e68c0eff66
defianttz <80181373+defianttz@users.noreply.github.com>
2023-05-18 20:54:19 -0700

    final code

--- quixo_game.py
+++ quixo_game.py
@@ -60,37 +60,6 @@ class Quixo(Game):
 
         return state.utility != 0 or len(state.moves) == 0
 
-    def terminal_test2(self, state):
-        board = state.board
-
-        # Check if the board is completely filled
-        if all(cell != ' ' for row in board for cell in row):
-            return True
-
-        # Check if there is a winner in any row
-        for row in board:
-            if len(set(row)) == 1 and row[0] != ' ':
-                return True
-
-        # Check if there is a winner in any column
-        for col in range(len(board[0])):
-            column = [board[row][col] for row in range(len(board))]
-            if len(set(column)) == 1 and column[0] != ' ':
-                return True
-
-        # Check if there is a winner in the main diagonal
-        diagonal = [board[i][i] for i in range(len(board))]
-        if len(set(diagonal)) == 1 and diagonal[0] != ' ':
-            return True
-
-        # Check if there is a winner in the anti-diagonal
-        anti_diagonal = [board[i][len(board) - i - 1] for i in range(len(board))]
-        if len(set(anti_diagonal)) == 1 and anti_diagonal[0] != ' ':
-            return True
-
-        return False
-
-
     # Define a function to print the current board state
     def print_board(self, board):
         """Print the current board state."""
@@ -132,7 +101,6 @@ class Quixo(Game):
         for j in range(len(board[0])):
             if all(board[i][j] == player for i in range(len(board))):
                 return 1
-
         return 0
 
     def check_diagonal_lr(self, board, player):
@@ -145,132 +113,6 @@ class Quixo(Game):
             return 1
         return 0
 
-    def compute_utility2(self, board, player):
-        """If the game is over, return the utility for player."""
-
-        if (self.check_rows(board, player) or
-            self.check_columns(board, player) or
-            self.check_diagonal_lr(board, player) or
-            self.check_diagonal_rl(board, player)):
-            return 10 if player == 'X' else -10
-        else:
-            return 0
-    def min_value(self, state):
-        if (self.terminal_test(state)):
-            score = self.utility(state,  state.to_move)
-            return score
-        #depth -= 1
-        global boards
-        boards += 1
-        v = np.inf
-        for action in self.actions(state):
-
-            v = min(v, self.max_value(self.result(state, action)))
-
-        return v
-
-    def max_value(self, state):
-        if (self.terminal_test(state)):
-            return self.utility(state, state.to_move)
-        global boards
-        boards += 1
-        v = -np.inf
-        for action in self.actions(state):
-            #print(f"     move={action} board={state.board} depth={depth} player={state.to_move}\n")
-            #print(f"     move1={action} board={state.board} player={state.to_move}\n utility={self.utility(state, state.to_move)}\n")
-
-            v = max(v, self.min_value(self.result(state, action)))
-            #print(f"     move2={action} board={state.board} player={state.to_move}\n utility={self.utility(state, state.to_move)}\n")
-
-        return v
-    def minmax_depth(self, state, game):
-        """Return the optimal action for the current player, using the Minimax algorithm."""
-        player = state.to_move
-        max_depth = 4
-        global boards
-        boards = 0
-        def min_value(state, alpha, beta, depth):
-            #winner_found = self.terminal_test(state)
-
-            if self.terminal_test(state):
-                return self.utility(state, player, depth)
-
-            global boards
-
-            boards += 1
-            # depth -= 1
-            v = np.inf
-            for a in self.actions(state):
-                # print(f"     move1={action} board={state.board} player={state.to_move}\n utility={self.utility(state, state.to_move)}\n")
-                v = min(v, max_value(self.result(state, a), alpha, beta, depth-1))
-
-                if v <= alpha:
-                    return v
-                beta = min(beta, v)
-            return v
-
-        def max_value(state, alpha, beta, depth):
-            if self.terminal_test(state):
-                return self.utility(state, player, depth)
-
-            global boards
-            boards+=1
-            v = -np.inf
-            for a in self.actions(state):
-
-                v = max(v, min_value(self.result(state, a), alpha, beta, depth-1))
-
-                if v >= beta:
-                    return v
-                alpha = max(alpha, v)
-            return v
-
-        bestScore = -np.inf
-        beta = np.inf
-        bestMove = None
-        for a in game.actions(state):
-            score = min_value(self.result(state, a), bestScore, beta,0)
-            #print(f"key={a} score={score}", end="")
-
-            if score > bestScore:
-                bestScore = score
-                bestMove = a
-            #print(f"  bestScore={bestScore} bestMove={bestMove} board={state.board}")
-
-        print(f"AI explored boards={boards}")
-        return bestMove
-
-    def minmax_decision(self, state, game):
-        """Return the optimal action for the current player, using the Minimax algorithm."""
-        player = state.to_move
-        depth = 5
-        global boards
-        boards = 0
-        #move = max(game.actions(state), key=lambda a: self.min_value(game.result(state, a), depth))
-        move = max(game.actions(state), key=lambda a: self.min_value(game.result(state, a)))
-        return move
-    def minmax_decision2(self, state, game):
-        """Return the optimal action for the current player, using the Minimax algorithm."""
-        player = state.to_move
-        depth = 0
-        bestScore = -np.inf
-        bestMove = None
-        #move = max(game.actions(state), key=lambda a: self.min_value(game.result(state, a), depth))
-        for move in game.actions(state):
-            #self.log_info(f"key={move} board={state.board} depth={0}\n")
-            score = self.min_value(game.result(state, move), depth)
-
-            if score < bestScore:
-                bestScore = score
-                bestMove = move
-
-        return bestMove
-        """if player == 'X':
-            return max(game.actions(state), key=lambda move: game.min_value(game.result(state, move)))
-        else:
-            return min(game.actions(state), key=lambda move: game.max_value(game.result(state, move)))
-        """
-
     def play_game(self):
         state = self.initial
         while True:
@@ -281,9 +123,8 @@ class Quixo(Game):
                 move = query_player(self, state)
                 #move = random_player(self, state)
             else:
-                #move = self.minmax_decision(state, self)
-                #move = alpha_beta_player(self, state)
-                move = self.minmax_depth(state, self)
+                move = alpha_beta_cutoff_search(state, self)
+
             state = self.result(state, move)
             print(f"{player} picked {move} utility={state.utility}\n")
 
@@ -292,50 +133,20 @@ class Quixo(Game):
                 print("Game over!")
                 return self.utility(state, state.to_move)
 
-
-
-
-    def play_game2(self, *players):
-        """Play an n-person, move-alternating game."""
-        state = self.initial
-        while True:
-            for player in players:
-                move = player(self, state)
-                print(f'\nplayer: {state.to_move} move:', move)
-                #print('\nplayer:', self.to_move(state))
-
-                state = self.result(state, move)
-                #print("resulting state: \nboard:  ", state.board)
-                print("available moves:  ", state.moves)
-                if self.terminal_test(state):
-                    print("------------------")
-                    self.display(state)
-
-                    return self.compute_utility(state.board, self.to_move(self.initial))
-
-
 if __name__ == '__main__':
     print("Let's play Quixo!")
     """board = [['X', 'X', 'X', 'O', ' '],
              ['O', 'X', 'O', ' ', ' '],
-             ['O', ' ', 'X', ' ', ' '],
              ['O', ' ', ' ', ' ', ' '],
-             ['O', ' ', ' ', ' ', 'X']]"""
+             ['O', ' ', ' ', ' ', ' '],
+             [' ', ' ', ' ', ' ', 'X']]"""
     """board = [['X', 'X', 'X', ' '],
              ['O', 'X', 'O', ' '],
              ['O', ' ', 'X', ' '],
              ['O', ' ', 'O', ' ']]"""
     q = Quixo()
-    #print(f"utility{q.compute_utility(board, 'X')}")
-    #print(f"utility{q.compute_utility(board, 'O')}")
-    #print(q.result(q.initial, (4,4)).utility)
-    #print(q.initial)
 
-    #utility = q.play_game(query_player, minmax_player) # computer moves first
     utility = q.play_game() # computer moves first
-
-
-
     end_time = time.time()
     print("\nGame over!")
     if utility >0:
@@ -346,4 +157,4 @@ if __name__ == '__main__':
         print("\nGame ended in a draw!")
     runtime = end_time - start_time
     print("Runtime:", runtime, "seconds")
-    #utility = q.play_game(query_player, minmax_player) # computer moves first
+
